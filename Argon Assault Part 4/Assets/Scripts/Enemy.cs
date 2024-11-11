230bda1019fea3ddf6ba67c703dec85bb7c25bcd
mcdani978 <mcdani978@morrisville.edu>
2024-11-11 13:50:23 -0500

    Public methods implemented for class  communication

--- Argon Assault Part 4/Assets/Scripts/Enemy.cs
+++ Argon Assault Part 4/Assets/Scripts/Enemy.cs
@@ -6,8 +6,27 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
 
-     void OnParticleCollision(GameObject other)
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
