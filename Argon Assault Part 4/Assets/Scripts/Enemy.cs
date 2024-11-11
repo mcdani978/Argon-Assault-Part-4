1a6900dcd62d8e6ea3cf393f43d640caed3288f9
mcdani978 <mcdani978@morrisville.edu>
2024-11-11 13:17:32 -0500

    Dynamic instantiation of enemy explosions

--- Argon Assault Part 4/Assets/Scripts/Enemy.cs
+++ Argon Assault Part 4/Assets/Scripts/Enemy.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private void OnParticleCollision(GameObject other)
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+
+     void OnParticleCollision(GameObject other)
     {
-        Debug.Log($"{name}I'm hit! by {other.gameObject.name}");
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
