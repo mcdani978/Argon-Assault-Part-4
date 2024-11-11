e33e3361dee0fce8bd4e44dd71ab1dabd9191e8d
mcdani978 <mcdani978@morrisville.edu>
2024-11-10 20:19:56 -0500

    Implemented collision handling with  triggers

---
+++ Argon Assault Part 4/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(this.name + "--Collider with--" + other.gameObject.name);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+    }
+}
